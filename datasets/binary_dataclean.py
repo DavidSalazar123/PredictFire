12943e8fa9d1b2533511251c3b3aa7e4e6473e48
DavidSalazar123 <davidsalazar200@gmail.com>
2022-12-25 20:02:22 -0500

    Final updates

--- datasets/binary_dataclean.py
+++ datasets/binary_dataclean.py
@@ -33,10 +33,8 @@ df.insert(2, "Holiday", 0)
 usholidays = holidays.US()
 df["Holiday"] = [1 if str(val).split()[0]
                  in usholidays else 0 for val in df["datetime"]]
-
 # Format 
 df = df.loc[:, list(df.columns[6:7])+list(df.columns[11:12])+list(df.columns[1:2])+list(df.columns[2:3])+list(df.columns[3:6])+list(df.columns[21:22])+list(df.columns[25:26])
        + list(df.columns[26:27])+list(df.columns[24:25])+list(df.columns[20:21])+list(df.columns[22:23])+list(df.columns[23:24])+list(df.columns[7:10])+list(df.columns[15:20])]
-
 # Save cleaned data set
-df.to_csv("./datasets/binarydataset.csv")
+df.to_csv("./binarydataset.csv")
